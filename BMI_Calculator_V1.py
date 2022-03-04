8a35ef873e7a6d6229d076be2904b96b98a45694
Abdelrehman Fouad <97409061+MaridWSH@users.noreply.github.com>
2022-03-04 17:35:31 +0200

    Simple BMI Calculator

--- BMI_Calculator_V1.py
+++ BMI_Calculator_V1.py
@@ -1,4 +1,4 @@
 weight = int( input("enter your Weight : ") )
 height = float( input("enter your Hight : ") )
 BMI =  (weight / (height **2 )) 
-print (f"Your Body Mass Index is : {BMI} kg/m2" )
+print (f"Your Body Mass Index is : {BMI} Kg/m2" )
